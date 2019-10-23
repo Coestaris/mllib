90ebf0eddf0bf5e9e50b924a011e7ac7d1fccdb1
Maxim <vk_vm@ukr.net>
2019-10-23 12:10:20 +0300

    Image processig

--- MLLib/AI/CNN/Layers/ConvolutionalLayer.cs
+++ MLLib/AI/CNN/Layers/ConvolutionalLayer.cs
@@ -3,7 +3,6 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 using ml.AIMath;
-using Size = ml.AIMath.Size;
 
 namespace ml.AI.CNN
 {
@@ -12,7 +11,7 @@ namespace ml.AI.CNN
         public readonly Size LayerSize;
         public Size KernelSize => Kernel.Size;
 
-        public readonly double[,] Data;
+        public double[,] Data;
         public readonly Matrix Kernel;
 
         public void PadData()
@@ -22,43 +21,28 @@ namespace ml.AI.CNN
 
         public void ForwardPass(double[,] data)
         {
-
         }
 
-        public ConvolutionalLayer(Bitmap bitmap, Matrix kernel)
+        public ConvolutionalLayer(Size size, Matrix kernel)
         {
+            LayerSize = size;
+            LayerSize.Width  += (int)(Math.Ceiling(kernel.Size.Width  / 2.0) * 2);
+            LayerSize.Height += (int)(Math.Ceiling(kernel.Size.Height / 2.0) * 2);
 
+            Kernel = kernel;
+            Data = new double[size.Width, size.Height];
         }
 
         public Bitmap ToBitmap()
         {
-            var bmp = new Bitmap(LayerSize.Width, LayerSize.Height, PixelFormat.Format8bppIndexed);
+            var bmp = new Bitmap(LayerSize.Width, LayerSize.Height);
+            for(var x = 0; x < bmp.Width; x++)
+            for(var y = 0; y < bmp.Height; y++)
+                bmp.SetPixel(x ,y, Color.FromArgb(
+                        (int)(Data[x, y] * 256),
+                        (int)(Data[x, y] * 256),
+                        (int)(Data[x, y] * 256)));
 
-            var palette = bmp.Palette;
-            var entries = palette.Entries;
-            for (var i = 0; i < 256; i++)
-            {
-                var b = Color.FromArgb((byte)i, (byte)i, (byte)i);
-                entries[i] = b;
-            }
-            bmp.Palette = palette;
-
-
-            var data = bmp.LockBits(new Rectangle(
-                    Point.Empty,
-                    new System.Drawing.Size(LayerSize.Width, LayerSize.Height)),
-                ImageLockMode.WriteOnly,
-                bmp.PixelFormat);
-
-            var pixelData = new byte[LayerSize.Height * LayerSize.Width];
-            Data.CopyTo(pixelData, 0);
-
-            var targetStride = data.Stride;
-            var scan0 = data.Scan0.ToInt64();
-            for (var y = 0; y < LayerSize.Height; y++)
-                Marshal.Copy(pixelData, y * LayerSize.Width, new IntPtr(scan0 + y * targetStride),  LayerSize.Width);
-
-            bmp.UnlockBits(data);
             return bmp;
         }
     }
--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Drawing;
 using ml.AI.CNN;
+using ml.AIMath;
 
 namespace Tests
 {
@@ -9,8 +11,20 @@ namespace Tests
 
         public static void Main(string[] args)
         {
-            var network = new ConvolutionalNeuralNetwork();
-            network.Layers.Add(new ConvolutionalLayer());
+            var bitmap = new Bitmap("img.png");
+            var kernel = new Matrix(3, 3,
+                new double[] {0,1,0,0,2,0,0,1,0});
+
+            var layer = new ConvolutionalLayer(
+                new Size(bitmap.Width, bitmap.Height),
+                kernel);
+
+            var data = new InputData(bitmap);
+            data.Pad(kernel);
+            data.ToBitmap().Save("padded.png");
+
+            layer.ForwardPass(data.Data);
+            //layer.ToBitmap().Save("result.png");
         }
     }
 }
